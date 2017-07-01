c520ae9a18352685a4780e233e7de8d1f2f1e374
haozheng <zhwtf123@gmail.com>
2017-07-01 16:38:08 -0700

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListActivity.java
@@ -0,0 +1,15 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by zhenghao on 2017-06-29.
+ */
+
+public class CrimeListActivity extends SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeListFragment();
+    }
+}
